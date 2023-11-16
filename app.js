d8503c823a80a1c28e1fb6fa3decbc61e8265e17
GuacamoleMole <104989112+GuacamoleMole@users.noreply.github.com>
2023-11-16 20:41:16 +0100

    cambios

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const morgan = require('morgan');
 
 var app = express();
 
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Ficheros estáticos
+app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //Ruta de registro
 app.get("/registroUsuario", function(request, response,next) {
@@ -29,12 +28,6 @@ app.get("/login", function(request, response,next) {
 });
 
 // TODO: todo esto hay que reorganizarlo
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
