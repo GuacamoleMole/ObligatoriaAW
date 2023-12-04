ebe9997043c1eb4a10ff182d947583e8299a3727
Alberto Ramos Suárez <bertoramos2001@gmail.com>
2023-12-04 22:04:28 +0100

    esqueleto pagina instalacion (falta estilo)

--- routes/instalacion.js
+++ routes/instalacion.js
@@ -3,7 +3,14 @@ const router = Router();
 
 //Ruta página instalación, donde se muestra la instalación en detalle y se permite reservarla
 router.get("/:id", function(req, res) {
+  datos = {};
   const id = req.params.id;
+  datos.id = id;
+  
+  if(req.session.user !== undefined){
+    datos.session = req.session.user;
+  }
+  
   res.status(200);
   // TODO: buscar el id y pasar el nombre e información de la instalación
   res.render("instalacion", {id});
