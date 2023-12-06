5639d3bcc29ca3f536d6b0da0e5dac89bd01e561
Samuel <samuelcalderon4343@gmail.com>
2023-12-06 14:08:59 +0100

    admin example

---
+++ database/admin.sql
@@ -0,0 +1,3 @@
+-- Contraseña: 12345
+-- Cambia imagen para que default sea null;
+INSERT INTO `UCM_AW_RIU_USU_Usuarios` (`nombre`, `apellidos`, `facultad`, `email`, `contraseña`, `curso`, `grupo`, `foto`, `rol`, `validado`) VALUES ('Luigi', 'Vampa', 'Informática', 'luigiV@ucm.es', '$2b$11$mjsBXIsUi6nGaMa31wYrI.t3Z2AIAS0BlspInxlgJVYsUrCnS0k2O', 6, 'I', NULL, 'admin', 1);
