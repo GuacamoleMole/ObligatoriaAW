e6c7440425914babdfca63b7fdb9b5141a1bbd4b
Alberto Ramos Suárez <bertoramos2001@gmail.com>
2023-12-12 22:51:56 +0100

    mensajes casi acabado (falta cargar usuarios facultad y enviar mensaje)

--- database/queries/menQueries.js
+++ database/queries/menQueries.js
@@ -4,8 +4,7 @@ module.exports = {
   enviarMensaje: `INSERT INTO UCM_AW_RIU_MEN_Mensajes(id_origen, id_dest, contenido, asunto)
   VALUES (?,?,?,?)`,
 
-  //TODO no se si estan bien las queries
-  mensajesRecibidios: 'SELECT  USU.email, MEN.contenido, MEN.asunto FROM UCM_AW_RIU_MEN_Mensajes MEN JOIN UCM_AW_RIU_USU_Usuarios USU ON MEN.id_origen = USU.id WHERE MEN.id_dest = ?',
+  mensajesRecibidos: 'SELECT  USU.email, USU.nombre, MEN.contenido, MEN.asunto FROM UCM_AW_RIU_MEN_Mensajes MEN JOIN UCM_AW_RIU_USU_Usuarios USU ON MEN.id_origen = USU.id WHERE MEN.id_dest = ?',
   
 };
   
--- routes/mensajes.js
+++ routes/mensajes.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const DAOMensajes = require('../database/DAOMensajes')
 const daoMensajes = new DAOMensajes();
+const DAOUsuario = require('../database/DAOUsuario')
+const daoUsuario = new DAOUsuario();
 const router = Router();
 
 //Ruta página instalación, donde se muestra la instalación en detalle y se permite reservarla
@@ -9,27 +11,35 @@ router.get("/:id", function(req, res, next) {
   const id = req.params.id;
   datos.id = id;
 
-//   daoMensajes.mensajesRecibidos(id, (err, mensajes) => {
-//     if (err) {
-//         next(err);
-//     } else {
-//         if (mensajes === undefined) {
-//             next();
-//         } else {
-//             datos.mensajes = mensajes;
-//         }
-//         if(req.session.user !== undefined){
-//             datos.session = req.session.user;
-//         }
-//         res.status(200);
-//         res.render("mensajes", {datos});
-//     }
-//   });
-if(req.session.user !== undefined){
-    datos.session = req.session.user;
-}
-res.status(200);
-res.render("mensajes", {datos});
+  daoMensajes.mensajesRecibidos(id, (err, mensajes) => {
+    if (err) {
+        next(err);
+    } else {
+      if (mensajes === undefined) {
+          next();
+      } else {
+          datos.mensajes = mensajes;
+      }
+
+      daoUsuario.buscarValidados((err, usuarios) => {
+          if (err) {
+              next(err);
+          } else {
+              if (usuarios === undefined) {
+                  next();
+              } else {
+                  datos.usuarios = usuarios;
+                  console.log(usuarios);
+              }
+              if(req.session.user !== undefined) {
+                  datos.session = req.session.user;
+              }
+              res.status(200);
+              res.render("mensajes", {datos});
+          }
+      });
+    }
+  });
 });
 
 
