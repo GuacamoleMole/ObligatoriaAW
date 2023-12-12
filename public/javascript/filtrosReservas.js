bc2a46c4f029368195a8a651bd1cc0e029fc526a
Alberto Ramos Suárez <bertoramos2001@gmail.com>
2023-12-12 21:03:38 +0100

    historial de reservas y filtros

--- public/javascript/filtrosReservas.js
+++ public/javascript/filtrosReservas.js
@@ -4,27 +4,37 @@ $(document).ready(function () {
         let facultadFilter = $('#facultad').val().toLowerCase();
         let instalacionFilter = $('#instalacion').val().toLowerCase();
         let emailFilter = $('#email').val().toLowerCase();
-        let horaInicioFilter = $('#horaInicio').val().toLowerCase();
-        let horaFinFilter = $('#horaFin').val().toLowerCase();
+        let horaInicioFilter = $('#horaInicio').val();
+        let horaFinFilter = $('#horaFin').val();
+        let fechaFilter = $('#fecha').val();
+        fechaFilter = new Date(fechaFilter).toLocaleDateString();
 
         $('.resultadoBusqueda').each(function () {
             let facultad = $(this).find('.campoUsuarioFacultad').text().toLowerCase();
-            let instalacion = $(this).find('.campoInstalacion').text().toLowerCase();
+            let instalacion = $(this).find('.campoUsuarioInstalacion').text().toLowerCase();
             let email = $(this).find('.campoUsuarioEmail').text().toLowerCase();
+            let horaInicio = $(this).find('.campoUsuarioHoraInicio').text();
+            let horaFin = $(this).find('.campoUsuarioHoraFin').text();
+            let fecha = $(this).find('.campoUsuarioFecha').text();
 
             // Comprobar si el item actual coincide con los criterios de filtrado
-            if ((instalacion.indexOf(instalacionFilter) > -1 || instalacionFilter === '' || instalacionFilter === 'todas') &&
+            if ((instalacion.indexOf(instalacionFilter) > -1 || instalacionFilter === 'todas') &&
                 (email.indexOf(emailFilter) > -1 || emailFilter === '') &&
-                (facultad.indexOf(facultadFilter) > -1 || facultadFilter === '' || facultadFilter === 'todos')) {
+                (facultad.indexOf(facultadFilter) > -1 || facultadFilter === 'todos') &&
+                (horaInicio.indexOf(horaInicioFilter) > -1 || horaInicioFilter === '') &&
+                (horaFin.indexOf(horaFinFilter) > -1 || horaFinFilter === '') &&
+                (fecha.indexOf(fechaFilter) > -1 || fechaFilter === '' || fechaFilter == 'Invalid Date')) {
                 $(this).parent().show(); // Enseñar el resultado si coincide
             } else {
                 // Si no coincide, escondemos a su padre para que se muestren en orden
+                console.log(fechaFilter);
+                console.log(fecha);
                 $(this).parent().hide(); // Esconder el resultado si no coincide
             }
         });
     }
 
     // Añadimos el evento de filtrado a los campos de búsqueda
-    $('#email').on('input', filterResults);
-    $('#facultad', '#instalacion').on('change', filterResults);
+    $('#email, #horaInicio, #horaFin').on('input', filterResults);
+    $('#facultad, #instalacion, #fecha').on('change', filterResults);
 });
